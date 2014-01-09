580e82d00f95b3bc1aee38266e2fc9389f250c7a
xPaw <github@xpaw.ru>
2014-01-10 01:49:49 +0200

    Use jQuery.unveil on all items page (with some what retina support)

--- views/all_items.view.php
+++ views/all_items.view.php
@@ -11,6 +11,8 @@ class all_items_view extends view
 		$this->tab = "all_items";
 		$this->title = "All TF2 Items";
 		
+		$this->head = '<script>(function($){$.fn.unveil=function(threshold,callback){var $w=$(window),th=threshold||0,retina=window.devicePixelRatio>1,attrib=retina?"data-src-retina":"data-src",images=this,loaded;this.one("unveil",function(){var source=this.getAttribute(attrib);source=source||this.getAttribute("data-src");if(source){this.setAttribute("src",source);if(typeof callback==="function")callback.call(this);}});function unveil(){var inview=images.filter(function(){var $e=$(this),wt=$w.scrollTop(),wb=wt+$w.height(),et=$e.offset().top,eb=et+$e.height();return eb>=wt-th&&et<=wb+th;});loaded=inview.trigger("unveil");images=images.not(loaded);}$w.scroll(unveil);$w.resize(unveil);unveil();return this;};})(window.jQuery);$(document).ready(function(){$("img").unveil(166);});</script>';
+		
 		$b = new backpack(false);
 		
 		foreach($schema['items'] as $i)
